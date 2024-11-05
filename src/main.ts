21673378c39ee8338757f67746a1e197b09e9700
Admin <s_vladimir902@mail.ru>
2024-11-05 15:16:41 +0600

    First commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
+  await app.listen(3000);
+}
+bootstrap();
